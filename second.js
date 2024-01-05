473bcc48309108c7df879f206dec0aae899128e9
Tilak_Shelke <147398031+tilakshelke08@users.noreply.github.com>
2024-01-05 23:47:01 +0530

    basics

--- second.js
+++ second.js
@@ -1,3 +1,18 @@
-var a;
-a=150;
-alert(a);
+let score= prompt("enter the score ");
+
+if(100<=score && score>=80 ){
+  console.log("Grade A");
+  
+}
+else if(79<=score && score>=60)
+{
+  console.log("Grade B");
+  
+}
+else if(50<=score && score>=39){
+  console.log("Grade C");
+}
+else
+{
+  console.log("Grade D");
+}
---
+++ third.js
@@ -0,0 +1,7 @@
+let Name="TaimurSuperstar";
+let size=0;
+for(let i of Name){
+  console.log("i=",i)
+  size++;
+}
+console.log(size);
