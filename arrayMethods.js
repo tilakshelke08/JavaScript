85424fd84e66e82a5bc6f01d08dc0371ba262bb3
Tilak_Shelke <147398031+tilakshelke08@users.noreply.github.com>
2024-01-11 00:45:47 +0530

    basics

---
+++ arrayMethods.js
@@ -0,0 +1,13 @@
+let fooditems=["potato","apple","tomato","mango"];
+console.log(fooditems);
+//push
+console.log(fooditems.push("strawberry"));
+console.log(fooditems);
+//pop
+console.log(fooditems.pop());
+console.log(fooditems);
+console.log(fooditems.pop());
+console.log(fooditems);
+
+//tpstrings
+console.log(fooditems.toString());
---
+++ arrowfunctions.js
@@ -0,0 +1,17 @@
+function addition(a,b){
+  return a+b;
+
+}
+//arrow addition     // arror function is right side part not a left side part 
+const arrowaddition=( a ,b )  => {
+
+console.log(a+b);
+}
+ //multi
+function multi(c,d){
+  return c*d;
+}
+//arrowmulti
+const arrowMul=(a,b)=>{
+  console.log(a*b);
+} 
