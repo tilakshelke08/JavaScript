4ff294e1945a4a7856189f74dc7394d0f1876bec
Tilak_Shelke <147398031+tilakshelke08@users.noreply.github.com>
2024-01-16 02:04:10 +0530

    Add files via upload

---
+++ student_regi.js
@@ -0,0 +1,7 @@
+let  Student_regi={
+  name:"Tilak Shelke",
+  city:"Nagpur ",
+  clg:"Gh Raisoni College of Engineering Nagpur "
+
+}
+console.log(`the Student Registration data: ${Student_regi.name}${Student_regi.clg}${Student_regi.clg}`);
